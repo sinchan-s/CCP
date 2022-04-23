6fd0330af2f6c5f47b1c54b54d7bca5a026f45cc
Sinchan <sinchan.tex@gmail.com>
2022-04-24 00:28:52 +0530

    html + css updated

--- client/app.js
+++ client/app.js
@@ -1,39 +1,55 @@
-function getBathValue() {
-  var uiBathrooms = document.getElementsByName("uiBathrooms");
-  for(var i in uiBathrooms) {
-    if(uiBathrooms[i].checked) {
+function getRodValue() {
+  var uiRod = document.getElementsByName("uiRod");
+  for(var i in uiRod) {
+    if(uiRod[i].checked) {
         return parseInt(i)+1;
     }
   }
   return -1; // Invalid Value
 }
 
-function getBHKValue() {
-  var uiBHK = document.getElementsByName("uiBHK");
-  for(var i in uiBHK) {
-    if(uiBHK[i].checked) {
+function getMeshValue() {
+  var uiMesh = document.getElementsByName("uiMesh");
+  for(var i in uiMesh) {
+    if(uiMesh[i].checked) {
         return parseInt(i)+1;
     }
   }
   return -1; // Invalid Value
 }
 
-function onClickedEstimatePrice() {
-  console.log("Estimate price button clicked");
-  var sqft = document.getElementById("uiSqft");
-  var bhk = getBHKValue();
-  var bathrooms = getBathValue();
-  var location = document.getElementById("uiLocations");
-  var estPrice = document.getElementById("uiEstimatedPrice");
+function getPrintValue() {
+  var uiMesh = document.getElementsByName("uiPrint");
+  for(var i in uiPrint) {
+    if(uiPrint[i].checked) {
+        return parseInt(i)+1;
+    }
+  }
+  return -1; // Invalid Value
+}
+
+function onClickedEstimateColor() {
+  console.log("Estimate Consumption button clicked !!");
+  var meter = document.getElementById("uiMeter");
+  var cover = document.getElementById("uiCover");
+  var speed = document.getElementById("uiSpeed");
+  var rod = getRodValue();
+  var mesh = getMeshValue();
+  var print = getPrintValue();
+  var article = document.getElementById("uiArticle");
+  var estConsum = document.getElementById("uiEstimatedConsumption");
 
   //var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
   var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
 
   $.post(url, {
-      total_sqft: parseFloat(sqft.value),
-      bhk: bhk,
-      bath: bathrooms,
-      location: location.value
+      meters: meter,
+      cover: cover,
+      speed: speed,
+      rod: rod,
+      mesh: mesh,
+      print: print,
+      article: article.value
   },function(data, status) {
       console.log(data.estimated_price);
       estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
