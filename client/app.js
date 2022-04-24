8a41242a17f7f37e1eaedeb9f6942d315a98db78
Sinchan <sinchan.tex@gmail.com>
2022-04-24 15:31:59 +0530

    missing features + functions added

--- client/app.js
+++ client/app.js
@@ -1,3 +1,33 @@
+function getHitsValue() {
+  var uiHits = document.getElementsByName("uiHits");
+  for(var i in uiHits) {
+    if(uiHits[i].checked) {
+        return parseInt(i)+1;
+    }
+  }
+  return -1; // Invalid Value
+}
+
+function getCHitsValue() {
+  var uiCHits = document.getElementsByName("uiCHits");
+  for(var i in uiCHits) {
+    if(uiCHits[i].checked) {
+        return parseInt(i)+1;
+    }
+  }
+  return -1; // Invalid Value
+}
+
+function getMachineValue() {
+  var uiMachine = document.getElementsByName("uiMachine");
+  for(var i in uiMachine) {
+    if(uiMachine[i].checked) {
+        return parseInt(i)+1;
+    }
+  }
+  return -1; // Invalid Value
+}
+
 function getRodValue() {
   var uiRod = document.getElementsByName("uiRod");
   for(var i in uiRod) {
@@ -19,7 +49,7 @@ function getMeshValue() {
 }
 
 function getPrintValue() {
-  var uiMesh = document.getElementsByName("uiPrint");
+  var uiPrint = document.getElementsByName("uiPrint");
   for(var i in uiPrint) {
     if(uiPrint[i].checked) {
         return parseInt(i)+1;
@@ -28,48 +58,68 @@ function getPrintValue() {
   return -1; // Invalid Value
 }
 
-function onClickedEstimateColor() {
+function getFinishValue() {
+  var uiFinish = document.getElementsByName("uiFinish");
+  for(var i in uiFinish) {
+    if(uiFinish[i].checked) {
+        return parseInt(i)+1;
+    }
+  }
+  return -1; // Invalid Value
+}
+
+function onClickedEstimateConsumption() {
   console.log("Estimate Consumption button clicked !!");
   var meter = document.getElementById("uiMeter");
   var cover = document.getElementById("uiCover");
   var speed = document.getElementById("uiSpeed");
+  var viscosity = document.getElementById("uiViscosity");
+  var hits = getHitsValue();
+  var chits = getCHitsValue();
+  var machine = getMachineValue();
   var rod = getRodValue();
   var mesh = getMeshValue();
   var print = getPrintValue();
-  var article = document.getElementById("uiArticle");
+  var finish = getFinishValue();
+  var articles = document.getElementById("uiArticles");
   var estConsum = document.getElementById("uiEstimatedConsumption");
 
-  //var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
-  var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+  var url = "http://127.0.0.1:5000/predict_color_consum"; //Use this if you are NOT using nginx which is first 7 tutorials
+  //var url = "/api/predict_color_consum"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
 
-  $.post(url, {
-      meters: meter,
-      cover: cover,
-      speed: speed,
-      rod: rod,
-      mesh: mesh,
-      print: print,
-      article: article.value
+  $.post(url, {  
+    articles: articles.value,
+    finish: finish,
+    print: print,
+    cover: cover,
+    meters: meter,
+    mesh: mesh,
+    rod: rod,
+    speed: speed,
+    hits: hits,
+    chits: chits,
+    viscosity: viscosity,
+    machine: machine
   },function(data, status) {
-      console.log(data.estimated_price);
-      estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+      console.log(data.estimated_consum);
+      estConsum.innerHTML = "<h2>" + data.estimated_consum.toString() + " Kgs</h2>";
       console.log(status);
   });
 }
 
 function onPageLoad() {
   console.log( "document loaded" );
-  //var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
-  var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+  var url = "http://127.0.0.1:5000/get_article_names"; // Use this if you are NOT using nginx which is first 7 tutorials
+  //var url = "/api/get_article_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
   $.get(url,function(data, status) {
-      console.log("got response for get_location_names request");
+      console.log("got response for get_article_names request");
       if(data) {
-          var locations = data.locations;
-          var uiLocations = document.getElementById("uiLocations");
-          $('#uiLocations').empty();
-          for(var i in locations) {
-              var opt = new Option(locations[i]);
-              $('#uiLocations').append(opt);
+          var articles = data.articles;
+          var uiArticles = document.getElementById("uiArticles");
+          $('#uiArticles').empty();
+          for(var i in articles) {
+              var opt = new Option(articles[i]);
+              $('#uiArticles').append(opt);
           }
       }
   });
