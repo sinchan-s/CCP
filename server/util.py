1d662ef2105909dddcb3f43ec10f0b387df6fe57
Sinchan <sinchan.tex@gmail.com>
2022-04-24 15:33:52 +0530

    util file fixed

--- server/util.py
+++ server/util.py
@@ -3,22 +3,17 @@ import pickle
 import numpy as np
 
 __article = None
-__fibre = None
 __finish = None
 __prints = None
 __data_columns = None
 __model = None
 
 
-def get_estimated_consum(article,fibre,finish,prints,count,cover,meters,mesh,rod,speed,hits,c_hits,viscosity,machine):
+def get_estimated_consum(article,finish,prints,cover,meters,mesh,rod,speed,hits,c_hits,viscosity,machine):
     try:
         art_index = __data_columns.index(article.lower())
     except:
         art_index = -1
-    try:
-        fib_index = __data_columns.index(fibre.lower())
-    except:
-        fib_index = -1
     try:
         fin_index = __data_columns.index(finish.lower())
     except:
@@ -28,19 +23,17 @@ def get_estimated_consum(article,fibre,finish,prints,count,cover,meters,mesh,rod
     except:
         sty_index = -1
     x = np.zeros(len(__data_columns))
-    x[0] = count
-    x[1] = cover
-    x[2] = meters
-    x[3] = mesh
-    x[4] = rod
-    x[5] = speed
-    x[6] = hits
-    x[7] = c_hits
-    x[8] = viscosity
-    x[9] = machine
-    if art_index or fib_index or fin_index or sty_index > 0:
+    x[0] = cover
+    x[1] = meters
+    x[2] = mesh
+    x[3] = rod
+    x[4] = speed
+    x[5] = hits
+    x[6] = c_hits
+    x[7] = viscosity
+    x[8] = machine
+    if art_index or fin_index or sty_index > 0:
         x[art_index] = 1
-        x[fib_index] = 1
         x[fin_index] = 1
         x[sty_index] = 1
     return round(__model.predict([x])[0], 2)
@@ -49,10 +42,6 @@ def get_estimated_consum(article,fibre,finish,prints,count,cover,meters,mesh,rod
 def get_article_names():
     global __article
     return __article
-
-def get_fibre_names():
-    global __fibre
-    return __fibre
 
 def get_finish_names():
     global __finish
@@ -65,17 +54,15 @@ def get_prints_names():
 def load_saved_artifacts():
     print('loading saved artifacts...start')
     global __article
-    global __fibre
     global __prints
     global __finish
     global __data_columns
     global __model
     with open('./artifacts/columns.json', 'r') as f:
         __data_columns = json.load(f)['data_columns']
-        __article = __data_columns[10:107]
-        __fibre = __data_columns[107:117]
-        __finish = __data_columns[117:120]
-        __prints = __data_columns[120:]
+        __article = __data_columns[9:106]
+        __finish = __data_columns[106:109]
+        __prints = __data_columns[109:]
 
     with open('./artifacts/color_model.pickle', 'rb') as f:
         __model = pickle.load(f)
@@ -85,7 +72,6 @@ def load_saved_artifacts():
 if __name__ == "__main__":
     load_saved_artifacts()
     print(get_article_names())
-    print(get_fibre_names())
     print(get_finish_names())
     print(get_prints_names())
-    print(get_estimated_consum('18006','Ct','none','reactive',30,40,1000,125,8,30,1,1,70,2))
+    print(get_estimated_consum('18006','none','reactive',40,1000,125,8,30,1,1,70,2))
